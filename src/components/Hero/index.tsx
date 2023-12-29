94c14bb7ca9f5c807b7be3fe8b27a4d4f92b2f9e
houssemELbahri <houssembahri1995@gmail.com>
2023-12-29 20:23:04 +0100

    working on hero section

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,5 +1,4 @@
-import { MdDatasetLinked, MdMenu } from 'react-icons/md'
-import { FaFacebookF, FaGithub, FaLinkedinIn, FaTwitter } from 'react-icons/fa'
+import profile from '../../public/combo.png'
 import './hero.css'
 
 
@@ -7,11 +6,16 @@ import './hero.css'
 export const Hero = () => {
     return (
         <section className='hero'>
-            <p className='greeting'>Hello !</p>
-            <section className='description'>
-                I am Houssem Bahri, also known as @bahrouch, a front-end and mobile developer living in Nabeul, Tunisia.
-                I enjoy using the React ecosystem, especially to develop iOS and Android native applications. I also like working on open source projects or giving public talks.
-            </section>
+            <div className='left-container'>
+                <p className='greeting'>Hello !</p>
+                <section className='description'>
+                    I am Houssem Bahri, also known as @bahrouch, a front-end and mobile developer living in Nabeul, Tunisia.
+                    I enjoy using the React ecosystem, especially to develop iOS and Android native applications. I also like working on open source projects or giving public talks.
+                </section>
+            </div>
+            <div className='right-container'>
+                <img className='image-container' src={profile} />
+            </div>
         </section>
     )
 }
