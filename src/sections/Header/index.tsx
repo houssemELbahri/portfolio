65449979370f529eada9dec33079c6cdd25e5bbd
houssemELbahri <houssembahri1995@gmail.com>
2024-01-04 14:53:02 +0100

    add dark mode

--- src/sections/Header/index.tsx
+++ src/sections/Header/index.tsx
@@ -1,4 +1,9 @@
 import './header.css'
+import { useEffect, useState } from 'react'
+import { IoMdFootball } from "react-icons/io";
+import { MdDarkMode } from "react-icons/md";
+import { LuSun } from "react-icons/lu";
+
 
 
 const LINKS = [
@@ -21,23 +26,60 @@ const LINKS = [
 
 
 export const Header = () => {
+    const [theme, setTheme] = useState<string>(localStorage.getItem("currentTheme") ?? "light")
+
+
+    useEffect(() => {
+        if (theme == "light") {
+            document.body.classList.remove("dark");
+            document.body.classList.add("light");
+        }
+        else {
+            document.body.classList.remove("light");
+            document.body.classList.add("dark");
+        }
+        document.body.classList.add(theme)
+    }, [theme])
+
+    const toggleTheme = () => {
+        localStorage.setItem("currentMode", theme == "dark" ? "light" : "dark")
+        setTheme(localStorage.getItem("currentMode"))
+    }
     return (
         <header className='header'>
             <div className='header-container'>
                 <p className='me'>@ElBahri</p>
-                <div>
-                    <ul className='networks flex'>
-                        {LINKS.map(el => {
-                            return (
-                                <li key={el.id}>
-                                    <a className='linkTo' href={el.link} target='_blank' >
-                                        {el.name}
-                                    </a>
-                                </li>
-                            )
-                        })}
-                    </ul>
+                <div className='flex'>
+                    <div>
+                        <ul className='networks flex'>
+                            {LINKS.map(el => {
+                                return (
+                                    <li key={el.id}>
+                                        <a className='linkTo' href={el.link} target='_blank' >
+                                            {el.name}
+                                        </a>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                    <div className='mode'>
+                        <button
+                            className='button-mode'
+                            onClick={toggleTheme}>
+                            <h1 className='mode-icon'>
+                                {theme == "light" ?
+                                    <LuSun style={{color:'#000000',padding:"5px",width:"25px",height:"25px",borderRadius:"30px"}} />
+                                    :
+                                    <MdDarkMode style={{color:'#FFFFFF',padding:"5px",width:"25px",height:"25px",backgroundColor:'#000000',borderRadius:"30px"}} />
+                                }
+                            </h1>
+                        </button>
+                    </div>
                 </div>
+                {/* <div className='mode'>
+                <button onClick={toggleTheme}>{theme}</button>
+                </div> */}
             </div>
         </header>
     )
--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -9,7 +9,7 @@ export const Hero = () => {
             <div className='hero-container'>
                 <div className='left-container'>
                     <p className='greeting'>Hello !</p>
-                    <section className='description'>
+                    <section className='special-description'>
                         I am <strong>Houssem Bahri</strong> , also known as <strong>@ElBahri</strong>, a front-end and mobile developer living in Nabeul, Tunisia.
                         I enjoy using the React ecosystem, especially to develop iOS and Android  applications. I also like working on web projects .
                     </section>
